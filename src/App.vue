809e69995a55da171e1b5d901087ba29d8d9a77f
hhp-42 <562915295@qq.com>
2020-09-05 17:42:38 +0800

    分类更新3

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,79 @@
 <template>
   <div id="app">
     <router-view/>
-    
-    <!-- 底部Tab -->
-    <van-tabbar route  v-show="showNav">
-      <van-tabbar-item replace v-for="item in menu" :key="item.path" :to="item.path" :icon="item.icon">{{item.title}}</van-tabbar-item>
-    </van-tabbar>
+    <van-tabbar v-model="active" route v-show="showTabbar">
+      <van-tabbar-item :badge="item.name==='cart'?cartLength:''" :icon="item.icon" v-for="item in menu" :key="item.name" :to="item.path">{{item.text}}</van-tabbar-item>
+  </van-tabbar>
   </div>
 </template>
 <script>
-import Vue from "vue";
-import { Icon, Tabbar, TabbarItem,Dialog,Notify } from "vant";
-Vue.use(Icon);
+import Vue from 'vue'
+import {Button,Tabbar, TabbarItem,Tag, Image, ImagePreview, Field, Checkbox } from 'vant'
+Vue.use(Image);
+Vue.use(Button)
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
-Vue.use(Dialog);
-Vue.use(Notify);
+Vue.use(Tag);
+Vue.use(Field);
+Vue.use(Checkbox)
 export default {
   data(){
-    return{
-      menu:[
-        {
-          title:'首页',
-          icon:'wap-home-o',
-          path:'/home',
-        },
-        {
-          title:'发现',
-          icon:'search',
-          path:'/goods',
-        },
-        {
-          title:'购物车',
-          icon:'cart-o',
-          path:'/cart',
-        },
-        {
-          title:'我的',
-          icon:'contact',
-          path:'/mine',
-        },
-      ],
-      showNav:true
+    return {
+      active:0,
+      // showMenu:true,
+      menu:[{
+        name:'home',
+        path:'/home',
+        icon:'wap-home-o',
+        text:'首页'
+      },
+      {
+        name:'classify',
+        path:'/classify',
+        icon:'apps-o',
+        text:'分类'
+      },{
+        name:'cart',
+        path:'/cart',
+        icon:'shopping-cart-o',
+        text:'购物车'
+      },{
+        name:'mine',
+        path:'/mine',
+        icon:'user-o',
+        text:'我的'
+      }]
     }
+  },
+  computed:{
+    cartLength(){
+      return this.$store.state.cart.goodslist.length
+    },
+    showTabbar(){
+      return this.$store.state.common.showTabbar
+    }
+  },
+  created(){
+    // this.$store.dispatch('getCart');
+    console.log(this.$store)
   }
 }
 </script>
-<style>
-
+<style lang="scss">
+.price {
+  del {
+    color: #999;
+    margin-right: 5px;
+    &::before {
+      content: "￥";
+    }
+  }
+  span {
+    color: #f00;
+    &::before {
+      content: "￥";
+    }
+  }
+}
 </style>
+
